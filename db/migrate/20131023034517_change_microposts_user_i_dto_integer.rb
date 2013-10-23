1ffb7c467b836c69529bd02d24ac0b85685c1d3c
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-10-22 21:28:56 -0700

    Fixing microposts database

--- db/migrate/20131023034517_change_microposts_user_i_dto_integer.rb
+++ db/migrate/20131023034517_change_microposts_user_i_dto_integer.rb
@@ -1,5 +1,5 @@
 class ChangeMicropostsUserIDtoInteger < ActiveRecord::Migration
   def change
-    change_column :microposts, :user_id, :integer
+    change_column :microposts, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end

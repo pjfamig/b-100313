20a78df42ad7d69c725338d73cd6165792dce949
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-10-22 19:59:25 -0700

    Midway through feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,7 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -142,8 +143,17 @@ describe User do
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
     end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
-  
 end
 
 
